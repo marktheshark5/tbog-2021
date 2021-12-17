51c6acf9dbf646a70eef958fd22261b2acd38bc8
marktheshark5 <markwheaton5@gmail.com>
2021-12-16 22:12:58 -0800

    ADS1115-single-channel

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ UBC Concrete Toboggan Elec team repository
 
 Package dependecies are as followings:
 House keeping	 : sudo apt-get update
-		   sudo apt-get upgrade
-PySimpleGUI	 : sudo pip3 install pysimplegui
-circuitpython	 : sudo pip3 install adafruit-circuitpython-ahtx0
-Adafruit 	 : sudo pip3 install adafruit-python-shell
+		   		   sudo apt-get upgrade
+PySimpleGUI	 	 : sudo pip3 install pysimplegui
+Adafruit 	 	 : sudo pip3 install adafruit-python-shell
 Adafruit-Blinka	 : sudo pip3 install adafruit-blinka
+AHT20			 : sudo pip3 install adafruit-circuitpython-ahtx0
 ADXL345 python	 : sudo pip3 install adafruit-circuitpython-adxl34x
+ADS1115 python	 : sudo pip3 install adafruit-circuitpython-ads1x15
 
--- src/strain_gauge/strain_gauge.py
+++ src/strain_gauge/strain_gauge.py
@@ -1,9 +1,29 @@
-#!/usr/pin/python3
+#!/usr/bin/python3
 
 # code to interface with strain gauges
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 class strainGauge(): 
-    def readValue:
-        pass
+    def __init__(self):
+        # Create the I2C bus
+        i2c = busio.I2C(board.SCL, board.SDA)
+        # Create the ADC object using the I2C bus
+        self.adc = ADS.ADS1115(i2c)
+        # Create single-ended input on channel 0
+        self.chan = AnalogIn(self.adc, ADS.P0)
 
+    def readValue(self):
+        # Create differential input between channel 0
+        values = AnalogIn(self.adc, ADS.P0)
+        return values
 
+# # example implementation
+# sen = strainGauge()
+# while True:
+#     values = sen.readValue()
+#     print("{:>5}\t{:>5.3f}".format(values.value, values.voltage))
+#     time.sleep(1)
