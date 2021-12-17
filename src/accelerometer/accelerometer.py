5fb4c51397415225de4366f0c88415c12a421747
marktheshark <58120125+marktheshark5@users.noreply.github.com>
2021-12-16 21:00:31 -0800

    Adxl345 inital (#4)
    * aht20-interface-and-gui-integration
    
    * read me package dependency updates
    
    * receiving-values-adxl345
    
    * ADXL345-todo-message

---
+++ src/accelerometer/accelerometer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import time
+import board
+import adafruit_adxl34x
+
+# TODO: create a sudo-speedometer using this accelerometer. It won't be perfect but I think that it will be cool to try
+
+class adxl345_interface:
+    def __init__(self):
+        self.i2c = board.I2C()  # uses board.SCL and board.SDA
+        self.accelerometer = adafruit_adxl34x.ADXL345(self.i2c)
+
+    def read_values(self):
+        # gather values and return them as an array
+        raw_values = self.accelerometer.acceleration
+        # option to return as a dictionary
+        # values = {'x' : raw_values[0], 'y' : raw_values[1], 'z' : raw_values[2]}
+        return raw_values 
+
+# implementation example
+# adxl = adxl345_interface()
+# while True:
+#     values = adxl.read_values()
+#     print(values)
+#     #print("%f %f %f"%accelerometer.acceleration)
+#     time.sleep(1)
